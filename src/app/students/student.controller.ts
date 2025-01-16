8c603c31bf627d39119e5af89f360e0bf4a5d29e
carolinefreitasalegre <freitascaroline.92@gmail.com>
2025-01-16 09:34:05 -0400

    modified:   src/app/students/student.controller.ts

--- src/app/students/student.controller.ts
+++ src/app/students/student.controller.ts
@@ -1,33 +1,36 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
-// import { CreateStudentService } from './create-student/create-student.service';
-import { AllStudentsDto } from './dto/all-students';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { CreateStudentService } from './create-student/create-student.service';
 import { FindAllStudentService } from './find-all-student/find-all-student.service';
-import { FindStudentByIdDto } from './dto/find-student-by-id';
 import { FindStudentByIdService } from './find-student-by-id/find-student-by-id.service';
+import { CreateStudentDto } from './dto/create-student.dto';
+import { DeleteStudentService } from './delete-student/delete-student.service';
 
 @Controller('student')
 export class StudentController {
   constructor(
-    // private readonly createStudentService: CreateStudentService,
-    private readonly findAllStudents: FindAllStudentService,
+    private readonly createStudentService: CreateStudentService,
+    private readonly findAllStudentService: FindAllStudentService,
     private readonly findStudentByIdService: FindStudentByIdService,
+    private readonly deleteStudentServide: DeleteStudentService,
   ) {}
 
   @Get('all-students')
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  async findAll(@Query('student') student: string): Promise<AllStudentsDto[]> {
-    return await this.findAllStudents.execute();
+  async findAll() {
+    return await this.findAllStudentService.execute();
   }
 
-  @Get('find-student-by-id:document')
-  findStudentById(
-    @Param('student') student: string,
-  ): Promise<FindStudentByIdDto> {
-    return this.findStudentByIdService.execute(student);
+  @Get('find-student-by-id/:document')
+  async findStudentById(@Param('document') document: string) {
+    return await this.findStudentByIdService.execute(document);
   }
 
-  // @Post('cadastrar')
-  // createStudent(@Body() createStudentDto: CreateStudentDto) {
-  //   this.createStudentService.(createStudentDto);
-  // }
+  @Post('cadastrar')
+  async createStudent(@Body() data: CreateStudentDto) {
+    return await this.createStudentService.execute(data);
+  }
+
+  @Delete('deletar/:document')
+  async deleteStudent(@Param('document') document: string) {
+    return await this.deleteStudentServide.execute(document);
+  }
 }
